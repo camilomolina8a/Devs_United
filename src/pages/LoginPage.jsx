7ba1cb9e939d82e249726de47f2ee125ec70ca42
Camilo Molina <camilomolina8a@gmail.com>
2022-02-26 02:38:28 -0500

    (componente, estilos): Pagina de Login Creada y sus estilos listos.

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "./pages_styles/LoginPage.css";
+
+import logoDevsUnited from "../assets/logo-big-devsUnited.png";
+import logoGoogle from "../assets/logo-google.png";
+
+function LoginPage() {
+    return (
+        <div className="LoginPage-container">
+            <div className="LoginPage-left">
+                <img src={logoDevsUnited} alt="Logo Devs United" />
+            </div>
+
+            <div className="LoginPage-right">
+                <h1>Together, building the future</h1>
+
+                <h3>A community for developers to unlock & share new skills</h3>
+
+                <div className="sigin-container">
+                    <div className="signin-logo-google">
+                        <img src={logoGoogle} alt="Google Logo" />
+                    </div>
+
+                    <div className="signin-text">
+                        <p>Sign in with Google</p>
+                    </div>
+                </div>
+
+                <p className="text-footer">
+                    © 2022 Devs_United - <span>BETA</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WelcomePage() {
+    return (
+    <>
+        <h1>Welcome Page</h1>
+    </>
+    )
+}
+
+export default WelcomePage;
